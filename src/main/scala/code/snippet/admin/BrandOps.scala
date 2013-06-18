0430b7961b5b0a77848737f6488f7cfc4a2612c7
liujiuwu <liujiuwu@gmail.com>
2013-06-18 18:06:54 +0800

    m

--- src/main/scala/code/snippet/admin/BrandOps.scala
+++ src/main/scala/code/snippet/admin/BrandOps.scala
@@ -6,44 +6,88 @@ import scala.xml.Text
 import code.lib.BrandTypeHelper
 import code.lib.WebHelper
 import code.model.Brand
-import code.model.BrandStatus
 import code.model.User
 import code.snippet.MyPaginatorSnippet
 import code.snippet.TabMenu
 import net.liftweb.common.Box
 import net.liftweb.common.Box.box2Option
-import net.liftweb.common.Empty
 import net.liftweb.common.Full
+import net.liftweb.common.Loggable
 import net.liftweb.http.RequestVar
+import net.liftweb.http.SHtml._
 import net.liftweb.http.SHtml.ElemAttr.pairToBasic
-import net.liftweb.http.SHtml.link
 import net.liftweb.mapper.By
+import net.liftweb.mapper.BySql
+import net.liftweb.mapper.Cmp
 import net.liftweb.mapper.Descending
+import net.liftweb.mapper.IHaveValidatedThisSQL
 import net.liftweb.mapper.MaxRows
-import net.liftweb.mapper.NotBy
 import net.liftweb.mapper.OrderBy
+import net.liftweb.mapper.QueryParam
 import net.liftweb.mapper.StartAt
 import net.liftweb.util.Helpers.strToCssBindPromoter
 import net.liftweb.util.Helpers.strToSuperArrowAssoc
 
-object BrandOps extends TabMenu with MyPaginatorSnippet[Brand] {
+object BrandOps extends TabMenu with MyPaginatorSnippet[Brand] with Loggable {
+  private object typeRV extends RequestVar[Box[String]](Full("0"))
+  private object keywordRV extends RequestVar[String]("")
   object brandRV extends RequestVar[Brand](Brand.create)
-  //object userRV extends RequestVar[Box[User]](Empty)
 
   override def itemsPerPage = 10
-  override def count = {
-    userRV.is match {
-      case Full(user) => Brand.count(By(Brand.owner, user))
-      case _ => Brand.count()
+
+  private def queryParam = {
+    val keyword = keywordRV.is
+    val searchType = typeRV.is.openOrThrowException("no select search type")
+    logger.info("keyword=" + keyword + ",searchType=" + searchType)
+    if (keyword != "") {
+      if (searchType == "0") {
+        By(Brand.regNo, keyword)
+      } else if (searchType == "1") {
+        BySql("owner=" + keyword, IHaveValidatedThisSQL("charliechen", "2011-07-21"))
+      }
+    }else{
+      
     }
   }
+
+  override def count = {
+    val keyword = keywordRV.is
+    val searchType = typeRV.is.openOrThrowException("no select search type")
+    logger.info("keyword=" + keyword + ",searchType=" + searchType)
+    if (keyword != "") {
+      if (searchType == "0") {
+        Brand.count(By(Brand.regNo, keyword), StartAt(curPage * itemsPerPage), MaxRows(itemsPerPage), OrderBy(Brand.createdAt, Descending))
+      } else if (searchType == "1") {
+        Brand.count(BySql("owner=" + keyword, IHaveValidatedThisSQL("charliechen", "2011-07-21")), StartAt(curPage * itemsPerPage), MaxRows(itemsPerPage), OrderBy(Brand.createdAt, Descending))
+      } else {
+        Brand.count()
+      }
+    } else {
+      Brand.count()
+    }
+  }
+
   override def page = {
-    println(userRV.is + "=================")
-    userRV.is match {
+    /*val keyword = keywordRV.is
+    val searchType = typeRV.is.openOrThrowException("no select search type")
+    logger.info("keyword=" + keyword + ",searchType=" + searchType)
+
+    if (keyword != "") {
+      if (searchType == "0") {
+        Brand.findAll(By(Brand.regNo, keyword), StartAt(curPage * itemsPerPage), MaxRows(itemsPerPage), OrderBy(Brand.createdAt, Descending))
+      } else if (searchType == "1") {
+        Brand.findAll(BySql("owner=" + keyword, IHaveValidatedThisSQL("charliechen", "2011-07-21")), StartAt(curPage * itemsPerPage), MaxRows(itemsPerPage), OrderBy(Brand.createdAt, Descending))
+      } else {
+        Brand.findAll(StartAt(curPage * itemsPerPage), MaxRows(itemsPerPage), OrderBy(Brand.createdAt, Descending))
+      }
+    } else {*/
+      Brand.findAll(queryParam,StartAt(curPage * itemsPerPage), MaxRows(itemsPerPage), OrderBy(Brand.createdAt, Descending))
+   // }
+    /*userRV.is match {
       case Full(user) =>
         Brand.findAll(By(Brand.owner, user), StartAt(curPage * itemsPerPage), MaxRows(itemsPerPage), OrderBy(Brand.createdAt, Descending))
       case _ => Brand.findAll(StartAt(curPage * itemsPerPage), MaxRows(itemsPerPage), OrderBy(Brand.createdAt, Descending))
-    }
+    }*/
 
   }
 
@@ -91,6 +135,12 @@ object BrandOps extends TabMenu with MyPaginatorSnippet[Brand] {
       "#useDescn" #> brand.useDescn &
       "#descn" #> brand.descn &
       "#owner" #> brand.owner.getOwner.name
+  }
+
+  def search = {
+    val types = List("0" -> "注册号", "1" -> "用户ID")
+    "@type" #> select(types, typeRV.is, x => typeRV(Full(x))) &
+      "@keyword" #> textElem(keywordRV)
   }
 
 }
