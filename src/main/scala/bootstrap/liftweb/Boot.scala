7af27b4882388e04ce05d767a9ce1fd1ec81153d
haitun <haitun@ppsea.com>
2013-07-22 18:10:04 +0800

    encode

--- src/main/scala/bootstrap/liftweb/Boot.scala
+++ src/main/scala/bootstrap/liftweb/Boot.scala
@@ -69,6 +69,7 @@ class Boot extends Loggable {
     LiftRules.ajaxEnd = Full(() => LiftRules.jsArtifacts.hide("ajax-loader").cmd)
     LiftRules.early.append(_.setCharacterEncoding("utf-8"))
     LiftRules.htmlProperties.default.set((r: Req) => new Html5Properties(r.userAgent))
+    LiftRules.htmlProperties.default.set((r: Req) => new StarXHtmlInHtml5OutProperties(r.userAgent))
     //LiftRules.handleMimeFile = OnDiskFileParamHolder.apply
 
     LiftRules.loggedInTest = Full(
