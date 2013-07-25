a8c0c3ad80a817ecb064ba9dc062800514948548
liujiuwu <liujiuwu@gmail.com>
2013-07-25 08:35:17 +0800

    m

--- src/main/scala/code/model/Paginator.scala
+++ src/main/scala/code/model/Paginator.scala
@@ -1,9 +1,14 @@
 package code.model
 
-import net.liftweb.mapper.KeyedMapper
-import net.liftweb.mapper.CRUDify
+import net.liftweb.mapper.LongKeyedMetaMapper
+import net.liftweb.mapper._
 
-case class Paginator[T](total: Long, datas: Seq[T], pageNo: Long = 1, itemsPerPage: Long = 20) {
+case class PaginatorData[T](total: Long, datas: Seq[T], pageNo: Long = 1, itemsPerPage: Long = 20) {
   def totalPage = (total / itemsPerPage).toInt + (if (total % itemsPerPage > 0) 1 else 0)
-
 }
+
+trait Paginator[T] {
+  def paginator(page: Long, itemsPerPage: Int, by: QueryParam[Brand]*): PaginatorData[T]
+}
+
+  
