5a355b1f8362a2cd1ed747c2e2537db1b4d48ba6
liujiuwu <liujiuwu@gmail.com>
2013-08-01 23:06:04 +0800

    grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,71 +1,94 @@
 module.exports = function(grunt) {
-
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-	setting: {
-		less:'src/main/less',
-		css:'src/main/webapp/css',
-		js:'src/main/webapp/js',
-	},
-    clean: {
-	  build: {
-		src: ["build", "dist"]
-	  }
-	},
-	concat: { 
-      bootstrap: { 
-        src: ['<%=setting.less%>/bootstrap/js/*.js'], 
-        dest: 'build/bootstrap.js' 
-      } 
-    }, 
-	uglify: {
-      bootstrap: {
-        src: ['<%=concat.bootstrap.dest%>'],
-        dest: '<%=setting.js%>/bootstrap.min.js'
-      }
-    },
-	recess: {
-	  options: {
-		compile: true
-	  },
-	  compile: {
-		files: {
-			'build/bootstrap.css': ['<%=setting.less%>/bootstrap/less/bootstrap.less'],
-			'build/bootstrap-responsive.css':['<%=setting.less%>/bootstrap/less/responsive.less'],
-			//'build/font-awesome.css':['<%=setting.less%>/font-awesome/font-awesome.less'],
-			'build/font-awesome-ie7.css':['<%=setting.less%>/font-awesome/font-awesome-ie7.less']
-		}
-	  },
-	  min: {
-		options: {
-			compress: true
+	grunt.initConfig({
+		pkg : grunt.file.readJSON('package.json'),
+		setting : {
+			srcLess : 'src/main/less',
+			srcJs : 'src/main/javascript',
+			distCss : 'src/main/webapp/css',
+			distJs : 'src/main/webapp/js',
 		},
-		files: {
-			'<%=setting.css%>/bootstrap.min.css': ['<%=setting.less%>/bootstrap/less/bootstrap.less'],
-			'<%=setting.css%>/bootstrap-responsive.min.css':['<%=setting.less%>/bootstrap/less/responsive.less'],
-			//'<%=setting.css%>/font-awesome.min.css':['<%=setting.less%>/font-awesome/font-awesome.less'],
-            '<%=setting.css%>/font-awesome-ie7.min.css':['<%=setting.less%>/font-awesome/font-awesome-ie7.less']
-		}
-	  }
-	},
-	watch: {  
-	  recess: {
-		files: '<%=setting.less%>/**/*.less',
-		tasks: ['recess']
-	  }
-    }, 
-  });
+		clean : {
+			build : {
+				src : [ "build" ]
+			}
+		},
+		concat : {
+			bootstrap : {
+				src : [ '<%=setting.srcJs%>/bootstrap/*.js' ],
+				dest : 'build/bootstrap.js'
+			}
+		},
+		uglify : {
+			bootstrap : {
+				src : [ '<%=concat.bootstrap.dest%>' ],
+				dest : '<%=setting.distJs%>/bootstrap.min.js'
+			},
+			other : {
+				files : {
+					'<%=setting.distJs%>/jquery-1.10.2.min.js' : [ '<%=setting.srcJs%>/jquery-1.10.2.js' ],
+					'<%=setting.distJs%>/jquery.ui.widget.min.js' : [ '<%=setting.srcJs%>/jquery.ui.widget.js' ],
+					'<%=setting.distJs%>/bootbox.min.js' : [ '<%=setting.srcJs%>/bootbox.js' ],
+					'<%=setting.distJs%>/bootstrap-modal.min.js' : [ '<%=setting.srcJs%>/bootstrap-modal.js' ],
+					'<%=setting.distJs%>/bootstrap-modalmanager.min.js' : [ '<%=setting.srcJs%>/bootstrap-modalmanager.js' ],
+					'<%=setting.distJs%>/jquery.fileupload.min.js' : [ '<%=setting.srcJs%>/jquery.fileupload.js' ],
+					'<%=setting.distJs%>/jquery.iframe-transport.min.js' : [ '<%=setting.srcJs%>/jquery.iframe-transport.js' ],
+					'<%=setting.distJs%>/jquery.jcrop.min.js' : [ '<%=setting.srcJs%>/jquery.jcrop.js' ],
+					'<%=setting.distJs%>/application.min.js' : [ '<%=setting.srcJs%>/application.js' ]
+				}
+			}
+		},
+		recess : {
+			options : {
+				compile : true
+			},
+			compile : {
+				files : {
+					'build/bootstrap.css' : [ '<%=setting.srcLess%>/bootstrap/bootstrap.less' ],
+					'build/bootstrap-responsive.css' : [ '<%=setting.srcLess%>/bootstrap/responsive.less' ],
+					'build/font-awesome-ie7.css' : [ '<%=setting.srcLess%>/font-awesome/font-awesome-ie7.less' ],
+					'build/application.min.css' : [ '<%=setting.srcLess%>/application.less' ],
+					'build/admin.min.css' : [ '<%=setting.srcLess%>/admin.less' ]
+				}
+			},
+			min : {
+				options : {
+					compress : true
+				},
+				files : {
+					'<%=setting.distCss%>/bootstrap.min.css' : [ '<%=setting.srcLess%>/bootstrap/bootstrap.less' ],
+					'<%=setting.distCss%>/bootstrap-responsive.min.css' : [ '<%=setting.srcLess%>/bootstrap/responsive.less' ],
+					'<%=setting.distCss%>/font-awesome-ie7.min.css' : [ '<%=setting.srcLess%>/font-awesome/font-awesome-ie7.less' ],
+					'<%=setting.distCss%>/bootstrap-modal.min.css' : [ '<%=setting.srcLess%>/bootstrap-modal.css' ],
+					'<%=setting.distCss%>/jquery.fileupload-ui.min.css' : [ '<%=setting.srcLess%>/jquery.fileupload-ui.css' ],
+					'<%=setting.distCss%>/jquery.jcrop.min.css' : [ '<%=setting.srcLess%>/jquery.jcrop.css' ],
+				}
+			},
+			myLess : {
+				options : {
+					compress : true
+				},
+				files : {
+					'<%=setting.distCss%>/application.min.css' : [ '<%=setting.srcLess%>/application.less' ],
+					'<%=setting.distCss%>/admin.min.css' : [ '<%=setting.srcLess%>/admin.less' ]
+				}
+			}
+		},
+		watch : {
+			recess : {
+				files : [ '<%=setting.srcLess%>/application.less', '<%=setting.srcLess%>/admin.less' ],
+				tasks : [ 'recess:myLess' ]
+			}
+		},
+	});
 
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-recess');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-recess');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-
-
-  grunt.registerTask('dist-js', ['clean','concat','uglify']);
-  grunt.registerTask('dist-css', ['recess']);
-  grunt.registerTask('default', ['dist-js','dist-css']);
+	grunt.registerTask('dist-js', [ 'concat', 'uglify' ]);
+	grunt.registerTask('dist-css', [ 'recess' ]);
+	grunt.registerTask('default', [ 'clean', 'dist-js', 'dist-css' ]);
 
 };
