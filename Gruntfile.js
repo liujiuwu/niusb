1f2a3e422fdcb97f2d15645ab4dc4ca74822cd76
haitun <haitun@ppsea.com>
2013-08-01 15:33:24 +0800

    grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,7 +14,7 @@ module.exports = function(grunt) {
 	},
 	concat: { 
       bootstrap: { 
-        src: ['src/bootstrap/js/*.js'], 
+        src: ['<%=setting.less%>/bootstrap/js/*.js'], 
         dest: 'build/bootstrap.js' 
       } 
     }, 
@@ -24,31 +24,48 @@ module.exports = function(grunt) {
         dest: '<%=setting.js%>/bootstrap.min.js'
       }
     },
-	less: {
-	  bootstrap: {
+	recess: {
+	  options: {
+		compile: true
+	  },
+	  compile: {
+		files: {
+			'build/bootstrap.css': ['<%=setting.less%>/bootstrap/less/bootstrap.less'],
+			'build/bootstrap-responsive.css':['<%=setting.less%>/bootstrap/less/responsive.less'],
+			//'build/font-awesome.css':['<%=setting.less%>/font-awesome/font-awesome.less'],
+			'build/font-awesome-ie7.css':['<%=setting.less%>/font-awesome/font-awesome-ie7.less']
+		}
+	  },
+	  min: {
 		options: {
-		  yuicompress: true
+			compress: true
 		},
 		files: {
-		  "<%=setting.css%>/bootstrap.min.css": "<%=setting.less%>/bootstrap/less/bootstrap.less",
-		  "<%=setting.css%>/bootstrap-responsive.min.css":"<%=setting.less%>/bootstrap/less/responsive.less"
+			'<%=setting.css%>/bootstrap.min.css': ['<%=setting.less%>/bootstrap/less/bootstrap.less'],
+			'<%=setting.css%>/bootstrap-responsive.min.css':['<%=setting.less%>/bootstrap/less/responsive.less'],
+			//'<%=setting.css%>/font-awesome.min.css':['<%=setting.less%>/font-awesome/font-awesome.less'],
+            '<%=setting.css%>/font-awesome-ie7.min.css':['<%=setting.less%>/font-awesome/font-awesome-ie7.less']
 		}
 	  }
 	},
-	watch: { 
-      files: '<%=setting.less%>/**/*.less', 
-      tasks: 'less' 
+	watch: {  
+	  recess: {
+		files: '<%=setting.less%>/**/*.less',
+		tasks: ['recess']
+	  }
     }, 
   });
 
 
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-recess');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
 
-  grunt.registerTask('default', ['clean','concat','uglify','less']);
+  grunt.registerTask('dist-js', ['clean','concat','uglify']);
+  grunt.registerTask('dist-css', ['recess']);
+  grunt.registerTask('default', ['dist-js','dist-css']);
 
 };
