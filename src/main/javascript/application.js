79b4a326ba3037b2a3a681de0ca7f81025c75e52
haitun <haitun@ppsea.com>
2013-08-05 18:24:09 +0800

    js

--- src/main/javascript/application.js
+++ src/main/javascript/application.js
@@ -1,3 +1,22 @@
-$(function(){
-	
+function mtab(selId, activeIdx, activeCls, tabWidth) {
+	function activeTab(selId, activeIdx, activeCls, tabWidth) {
+		$(selId + " .js-rline").animate({
+			left : (activeIdx * tabWidth) + "px"
+		}, {
+			queue : false
+		});
+		$(selId + " .js-tab").removeClass(activeCls);
+		$(selId + " .js-tab-c").hide();
+
+		$(selId + " .js-tab:eq(" + activeIdx + ")").addClass(activeCls);
+		$(selId + " .js-tab-c:eq(" + activeIdx + ")").show();
+	}
+	$(selId + " .js-tab").hover(function() {
+		activeTab(selId, $(selId + " .js-tab").index($(this)), activeCls, tabWidth);
+	});
+	activeTab(selId, activeIdx, activeCls, tabWidth);
+}
+
+$(function() {
+
 })
