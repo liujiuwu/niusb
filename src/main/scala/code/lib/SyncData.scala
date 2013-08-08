e99f7f6b03b328946d2e1f0e956136c4bdac9f81
haitun <haitun@ppsea.com>
2013-08-08 18:25:24 +0800

    test

--- src/main/scala/code/lib/SyncData.scala
+++ src/main/scala/code/lib/SyncData.scala
@@ -13,10 +13,6 @@ import code.model.User
 import net.liftweb.common.Box
 import net.liftweb.common.Empty
 import net.liftweb.common.Full
-import net.liftweb.mapper.Schemifier
-import net.liftweb.db.DB
-import net.liftweb.db.DefaultConnectionIdentifier
-import code.model.MyDBVendor
 
 case class Kehu(id: Int, name: String, tel: String, tel1: String, qq: String, bz: String, indate: Date, sqname: String)
 case class Trademark(id: Int, name: String, pic: String, indate: Date, price: Double, range: String, category: Int, number: String, regdate: String, sell: Boolean, address: String, tel: String, fax: String, coname: String, email: String, lsqz: String, kehu_1id: Int)
@@ -24,25 +20,21 @@ case class Trademark(id: Int, name: String, pic: String, indate: Date, price: Do
 object SyncData {
   implicit val getTrademarkResult = GetResult(r => Trademark(r.nextInt, r.nextString, r.nextString, r.nextDate(), r.nextDouble(), r.nextString(), r.nextInt(), r.nextString(), r.nextString(), r.nextBoolean(), r.nextString(), r.nextString(), r.nextString(), r.nextString(), r.nextString(), r.nextString(), r.nextInt()))
   implicit val getKehuResult = GetResult(r => Kehu(r.nextInt, r.nextString, r.nextString, r.nextString(), r.nextString(), r.nextString(), r.nextDate(), r.nextString()))
+  //DB.defineConnectionManager(DefaultConnectionIdentifier, MyDBVendor)
+  //Schemifier.schemify(true, Schemifier.infoF _, User, Brand)
+  lazy val db = Database.forURL("jdbc:mysql://localhost:3306/haotm", "ppseaer", "ppseaer@ppsea.com", driver = "com.mysql.jdbc.Driver")
 
-  DB.defineConnectionManager(DefaultConnectionIdentifier, MyDBVendor)
-  Schemifier.schemify(true, Schemifier.infoF _, User, Brand)
-
-  val db = Database.forURL("jdbc:mysql://localhost:3306/haotm", "ppseaer", "ppseaer@ppsea.com", driver = "com.mysql.jdbc.Driver")
   /*if (args.length < 3) {
     println("use source dir , dist dir and data limit .")
     System.exit(0)
   }
 
   init(args(0), args(1), args(2).toInt)*/
-
+  //init
   def init() {
-    new Thread(new Runnable() {
-      def run() {
-        //SyncData.init("/alidata/haotm", "/alidata/niusb_upload_file", 2000)
-        syncKehu("/alidata/haotm", "/alidata/niusb_upload_file", 2000)
-      }
-    }).start();
+    //SyncData.init("/alidata/haotm", "/alidata/niusb_upload_file", 2000)
+    syncKehu("/alidata/haotm", "/alidata/niusb_upload_file", 2000)
+    //syncKehu("""e:\1\haotm""", """d:\haotm""", 2000)
   }
 
   def syncTrademark(sdir: String, ddir: String, limit: Int) = {
